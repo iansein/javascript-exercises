1c5caf79d3a9f4bd89760303127b0dba673a0e2f
Ian Sein <ianseinmichel@gmail.com>
2023-10-11 18:29:58 -0300

    Add exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeater) {
+  let finalWord = "";
+  if (repeater >= 0) {
+    for (let i = 0; i < repeater; i++) {
+      finalWord += word;
+    }
+    return finalWord;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
