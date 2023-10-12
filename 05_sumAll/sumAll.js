5516d05f17503a3631faedb320877c05fc969987
Ian Sein <ianseinmichel@gmail.com>
2023-10-12 00:11:04 -0300

    Add exercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (initialNumber, finalNumber) {
+  let finalSum = 0;
 
+  if (
+    initialNumber >= 0 &&
+    finalNumber >= 0 &&
+    typeof initialNumber === "number" &&
+    typeof finalNumber === "number"
+  ) {
+    if (initialNumber < finalNumber) {
+      for (let i = initialNumber; i <= finalNumber; i++) {
+        finalSum += i;
+      }
+    } else {
+      for (let i = finalNumber; i <= initialNumber; i++) {
+        finalSum += i;
+      }
+    }
+    return finalSum;
+  }
+
+  return "ERROR";
 };
 
 // Do not edit below this line
